587507224084429d1effc96aa02f6a581783f500
cshm <cshm225@gmail.com>
2022-03-28 11:30:26 +0900

    松岡作成

---
+++ Matsuoka.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Matsuoka{
+	public static void main(String[] args){
+		int num;
+		System.out.print("何段？>>");
+		num=new Scanner(System.in).nextInt();
+		for(int i=0;i<num;i++){
+			for(int j=0;j<i+1;j++){
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
+
