fac51d16d1716bded83f5016acba876677826f11
arukreid <sasaki3y98521j@gmail.com>
2022-03-28 14:58:02 +0900

    ひし形

--- SasakiStep.java
+++ SasakiStep.java
@@ -19,7 +19,22 @@ public class SasakiStep{
 		//X
 		for(int i=0;i<3;i++){
 			for(int j=0;j<3;j++){
-				System.out.print((j-i)%2==0?"X":" ");
+				System.out.print((j+i)%2==0?"X":" ");
+			}
+			System.out.println();
+		}
+		//ひし形
+
+		System.out.print("好きな奇数を入れてください>>");
+		int numNum = new Scanner(System.in).nextInt();
+		int num22 = (numNum-1)/2;
+		for(int i=0;i<numNum;i++){
+			for(int j=0;j<numNum;j++){
+				if(Math.abs(i-num22)+Math.abs(j-num22) == num22){
+					System.out.print("X");
+				}else{
+					System.out.print(" ");
+				}
 			}
 			System.out.println();
 		}
