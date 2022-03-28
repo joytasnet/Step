0e685ba14f3865dc0509c13bf1e18bd78e5b1531
shotasuzuki <shotasuzuki01@gmail.com>
2022-03-28 11:54:02 +0900

    ShotaMain作成

---
+++ ShotaMain.java
@@ -0,0 +1,15 @@
+import java. util.*;
+public class ShotaMain{
+	public static void main(String[] args){
+		System.out.println("なんだん>>");
+		int num =new Scanner(System.in).nextInt();
+
+		for(int i = 0; i< num+1; i++){
+			for(int j = 0; j<i; j++){
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
+
