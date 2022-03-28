3f66bbd1d4fff4fb65954aa6520d64011c64f73d
okaka <aki.na@live.jp>
2022-03-28 12:39:36 +0900

    何段？2追加

--- NagatsuMain.java
+++ NagatsuMain.java
@@ -4,7 +4,7 @@ public class NagatsuMain{
 		System.out.print("何段？>");
 		int n = new Scanner(System.in).nextInt();
 		for(int i=0;i<n;i++){
-			for(int j=0;j<(i+1);j++){
+			for(int j=0;j<(5-i);j++){
 				System.out.print("*");
 			}
 			System.out.println();
