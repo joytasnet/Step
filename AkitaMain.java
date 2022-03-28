cad7adc9b378b27fae37f58a5239ef666c0d2690
fibuwimo <ray-rem-craim-requiem@ezweb.ne.jp>
2022-03-28 12:41:12 +0900

    step逆追加

--- AkitaMain.java
+++ AkitaMain.java
@@ -16,7 +16,7 @@ public class AkitaMain{
 			for(int j = 0;j<i;j++){
 				System.out.print(" ");
 			}
-			for(int j = 0;j<5-i;j++){
+			for(int j = 0;j<n-i;j++){
 				System.out.print("*");
 			}
 			System.out.println();
